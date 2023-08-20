99f701aa7139e456c52c472681166bf49148866f
anamika991 <nainanamika168@gmail.com>
2023-08-20 23:46:07 +0530

    Updated Login and registration page

--- react-project/src/auth-form/LoginForm.js
+++ react-project/src/auth-form/LoginForm.js
@@ -1,31 +1,38 @@
   import React from "react";
   import { useState } from "react";
   import { Button, Grid, InputLabel, OutlinedInput, Stack } from "@mui/material";
-  import { useForm } from 'react-hook-form';
+  // import { useForm } from 'react-hook-form';
   import { SnackbarProvider, useSnackbar } from "notistack";
 
   const LoginForm = () => {
     const [showPassword, setShowPassword] = useState(false); //eslint-disable-line
-    const { register, handleSubmit, errors, setValue } = useForm();
+    const [allEntry, setAllEntry] = useState([]);
+    // const { register, handleSubmit, errors, setValue } = useForm();
     const { enqueueSnackbar } = useSnackbar(); 
 
-    const onSubmit = async (values) => {
-      try {
-        // Implement your login logic here
-        // For example, send the login request to your backend
-        // Handle success and failure accordingly
-      } catch (err) {
-        enqueueSnackbar("PLease check your credentials.", { variant: 'error' });
-        if (err.response && err.response.status === 401) {
-          setValue("email", "", { shouldValidate: true });
-        }
-      }
-    };
+    const [usesrLogin, setUserLogin] = useState({
+      email: '',
+      password: ''
+    })
+
+    const handleInput = (e) => {
+      const name =  e.target.name;
+      const value = e.target.value;
+      console.log(name , value);
+    }
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+// const newEntry  = {name: value}
+    }
+
+
+  
 
     return (
       <>
         <SnackbarProvider maxSnack={3}> 
-          <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit} >
             <Grid container spacing={3}>
               <Grid item xs={12}>
                 <Stack spacing={1}>
@@ -33,18 +40,14 @@
                     Email Address<span style={{ color: "red" }}>*</span>
                   </InputLabel>
                   <OutlinedInput
-                    id="email-login"
-                    type="email"
+                    id="email"
+                  type="text"
                     name="email"
+                    value={usesrLogin.email}
+                    onChange={handleInput}
                     autoComplete="off"
-                    {...register("email", { required: "Email is required" })}
                     fullWidth
                   />
-                  {/* {errors.email && (
-                    <FormHelperText error id="helper-text-email-login">
-                      {errors.email.message}
-                    </FormHelperText>
-                  )} */}
                 </Stack>
               </Grid>
               <Grid item xs={12}>
@@ -55,15 +58,12 @@
                   <OutlinedInput
                     fullWidth
                     id="password"
-                    type={showPassword ? "text" : "password"}
+                  type="text"
                     name="password"
-                    {...register("password", { required: "Password is required" })}
+                    value={usesrLogin.password}
+                    onChange={handleInput}
                   />
-                  {/* {errors.password && (
-                    <FormHelperText error id="helper-text-password-login">
-                      {errors.password.message}
-                    </FormHelperText>
-                  )} */}
+                 
                 </Stack>
               </Grid>
               <Grid item xs={12}>
